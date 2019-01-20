f9bd7f5a2d910e23ee838445bd8846733c08bfe8
John Koehring <john@koehring.com>
2019-01-20 16:36:56 -0700

    Fixed formatting. Added command to toggle limelight camera mode.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -17,6 +17,7 @@ import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.ToggleLimelight;
 import frc.robot.subsystems.ExampleSubsystem;
 
 /**
@@ -26,221 +27,249 @@ import frc.robot.subsystems.ExampleSubsystem;
  * creating this project, you must also update the build.gradle file in the
  * project.
  */
-public class Robot extends TimedRobot {
-  public static ExampleSubsystem m_subsystem = new ExampleSubsystem();
-  public static OI m_oi;
+public class Robot extends TimedRobot
+{
+	public static ExampleSubsystem m_subsystem = new ExampleSubsystem();
+	public static OI m_oi;
 
-  // The following deal with the Limelight vision processing camera:
-  private static NetworkTable limelightTable;
-  private static NetworkTableEntry limelightXPositionEntry;
-  private static NetworkTableEntry limelightYPositionEntry;
-  private static NetworkTableEntry limelightAreaEntry;
+	// The following deal with the Limelight vision processing camera:
+	public static NetworkTable limelightTable;
+	public static NetworkTableEntry limelightXPositionEntry;
+	public static NetworkTableEntry limelightYPositionEntry;
+	public static NetworkTableEntry limelightAreaEntry;
+	public static NetworkTableEntry limelightCamModeEntry;
+	public static NetworkTableEntry limelightLedModeEntry;
 
-  public static double limelightXPosition;
-  public static double limelightYPosition;
-  public static double limelightArea;
+	public static double limelightXPosition;
+	public static double limelightYPosition;
+	public static double limelightArea;
 
-  /**
-   * The following deal with the REV Digit Board:
-   */
-  // The board itself:
-  private REVDigitBoard digitBoard;
+	/**
+	 * The following deal with the REV Digit Board:
+	 */
+	// The board itself:
+	private REVDigitBoard digitBoard;
 
-  // Keeps track on when to refresh the voltage display:
-  private int voltageRefreshCounter = 0;
-  private static final int voltageRefreshCount = 10;
+	// Keeps track on when to refresh the voltage display:
+	private int voltageRefreshCounter = 0;
+	private static final int voltageRefreshCount = 10;
 
-  // Keeps track on when to refresh the POT display:
-  private int potRefreshCounter = 0;
-  private static final int potRefreshCount = 10;
+	// Keeps track on when to refresh the POT display:
+	private int potRefreshCounter = 0;
+	private static final int potRefreshCount = 10;
 
-  // REV digit board button states:
-  private boolean buttonA = false;
-  private boolean buttonB = false;
+	// REV digit board button states:
+	private boolean buttonA = false;
+	private boolean buttonB = false;
 
-  // User options selected via the REV digit board:
-  private static final String[] options = { "STAY", "SMPL", "LEFT", "RGHT" };
-  private int optionIndex = 0;
+	// User options selected via the REV digit board:
+	private static final String[] options =
+	{ "STAY", "SMPL", "LEFT", "RGHT" };
+	private int optionIndex = 0;
 
-  // Keeps track of game controller back button:
-  private boolean backButton = false;
+	// Keeps track of game controller back button:
+	private boolean backButton = false;
 
-  // Te following deal with the Limelight vision processing camera:
+	// Te following deal with the Limelight vision processing camera:
 
-  Command m_autonomousCommand;
-  SendableChooser<Command> m_chooser = new SendableChooser<>();
+	Command m_autonomousCommand;
+	SendableChooser<Command> m_chooser = new SendableChooser<>();
 
-  /**
-   * This function is run when the robot is first started up and should be used
-   * for any initialization code.
-   */
-  @Override
-  public void robotInit()
-  {
-    m_oi = new OI();
+	/**
+	 * This function is run when the robot is first started up and should be used
+	 * for any initialization code.
+	 */
+	@Override
+	public void robotInit()
+	{
+		m_oi = new OI();
 
-    // Set up digit board:
-    digitBoard = new REVDigitBoard();
+		// Set up digit board:
+		digitBoard = new REVDigitBoard();
 
-    // Set up Limelight network table access:
-    limelightTable = NetworkTableInstance.getDefault().getTable(RobotMap.limelightTableKey);
-    limelightXPositionEntry = limelightTable.getEntry(RobotMap.limelightXPositionKey);
-    limelightYPositionEntry = limelightTable.getEntry(RobotMap.limelightYPositionKey);
-    limelightAreaEntry = limelightTable.getEntry(RobotMap.limelightAreaKey);
+		// Set up Limelight network table access:
+		limelightTable = NetworkTableInstance.getDefault().getTable(RobotMap.limelightTableKey);
+		limelightXPositionEntry = limelightTable.getEntry(RobotMap.limelightXPositionKey);
+		limelightYPositionEntry = limelightTable.getEntry(RobotMap.limelightYPositionKey);
+		limelightAreaEntry = limelightTable.getEntry(RobotMap.limelightAreaKey);
+		limelightCamModeEntry = limelightTable.getEntry(RobotMap.limelightCamModeKey);
+		limelightLedModeEntry = limelightTable.getEntry(RobotMap.limelightLedModeKey);
 
-    m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
-    // chooser.addOption("My Auto", new MyAutoCommand());
-    SmartDashboard.putData("Auto mode", m_chooser);
-  }
+		m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
+		// chooser.addOption("My Auto", new MyAutoCommand());
+		// SmartDashboard.putData("Auto mode", m_chooser);
+		SmartDashboard.putData("Toggle Limelight", new ToggleLimelight());
+	}
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for
-   * items like diagnostics that you want ran during disabled, autonomous,
-   * teleoperated and test.
-   *
-   * <p>
-   * This runs after the mode specific periodic functions, but before LiveWindow
-   * and SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-    pollDigitBoard();
-    pollLimelight();
-  }
+	/**
+	 * This function is called every robot packet, no matter the mode. Use this for
+	 * items like diagnostics that you want ran during disabled, autonomous,
+	 * teleoperated and test.
+	 *
+	 * <p>
+	 * This runs after the mode specific periodic functions, but before LiveWindow
+	 * and SmartDashboard integrated updating.
+	 */
+	@Override
+	public void robotPeriodic()
+	{
+		pollDigitBoard();
+		pollLimelight();
+	}
 
-  /**
-   * This function is called once each time the robot enters Disabled mode. You
-   * can use it to reset any subsystem information you want to clear when the
-   * robot is disabled.
-   */
-  @Override
-  public void disabledInit() {
-  }
+	/**
+	 * This function is called once each time the robot enters Disabled mode. You
+	 * can use it to reset any subsystem information you want to clear when the
+	 * robot is disabled.
+	 */
+	@Override
+	public void disabledInit()
+	{
+	}
 
-  @Override
-  public void disabledPeriodic() {
-    Scheduler.getInstance().run();
-  }
+	@Override
+	public void disabledPeriodic()
+	{
+		Scheduler.getInstance().run();
+	}
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable chooser
-   * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
-   * remove all of the chooser code and uncomment the getString code to get the
-   * auto name from the text box below the Gyro
-   *
-   * <p>
-   * You can add additional auto modes by adding additional commands to the
-   * chooser code above (like the commented example) or additional comparisons to
-   * the switch structure below with additional strings & commands.
-   */
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_chooser.getSelected();
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select
+	 * between different autonomous modes using the dashboard. The sendable chooser
+	 * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+	 * remove all of the chooser code and uncomment the getString code to get the
+	 * auto name from the text box below the Gyro
+	 *
+	 * <p>
+	 * You can add additional auto modes by adding additional commands to the
+	 * chooser code above (like the commented example) or additional comparisons to
+	 * the switch structure below with additional strings & commands.
+	 */
+	@Override
+	public void autonomousInit()
+	{
+		m_autonomousCommand = m_chooser.getSelected();
 
-    /*
-     * String autoSelected = SmartDashboard.getString("Auto Selector", "Default");
-     * switch(autoSelected) { case "My Auto": autonomousCommand = new
-     * MyAutoCommand(); break; case "Default Auto": default: autonomousCommand = new
-     * ExampleCommand(); break; }
-     */
+		/*
+		 * String autoSelected = SmartDashboard.getString("Auto Selector", "Default");
+		 * switch(autoSelected) { case "My Auto": autonomousCommand = new
+		 * MyAutoCommand(); break; case "Default Auto": default: autonomousCommand = new
+		 * ExampleCommand(); break; }
+		 */
 
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.start();
-    }
-  }
+		// schedule the autonomous command (example)
+		if (m_autonomousCommand != null)
+		{
+			m_autonomousCommand.start();
+		}
+	}
 
-  /**
-   * This function is called periodically during autonomous.
-   */
-  @Override
-  public void autonomousPeriodic() {
-    Scheduler.getInstance().run();
-  }
+	/**
+	 * This function is called periodically during autonomous.
+	 */
+	@Override
+	public void autonomousPeriodic()
+	{
+		Scheduler.getInstance().run();
+	}
 
-  @Override
-  public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
-    }
-  }
+	@Override
+	public void teleopInit()
+	{
+		// This makes sure that the autonomous stops running when
+		// teleop starts running. If you want the autonomous to
+		// continue until interrupted by another command, remove
+		// this line or comment it out.
+		if (m_autonomousCommand != null)
+		{
+			m_autonomousCommand.cancel();
+		}
+	}
 
-  /**
-   * This function is called periodically during operator control.
-   */
-  @Override
-  public void teleopPeriodic() {
-    Scheduler.getInstance().run();
-  }
+	/**
+	 * This function is called periodically during operator control.
+	 */
+	@Override
+	public void teleopPeriodic()
+	{
+		Scheduler.getInstance().run();
+	}
 
-  /**
-   * This function is called periodically during test mode.
-   */
-  @Override
-  public void testPeriodic() {
-  }
+	/**
+	 * This function is called periodically during test mode.
+	 */
+	@Override
+	public void testPeriodic()
+	{
+	}
 
-  /**
-   * Polls the digit board.
-   */
-  private void pollDigitBoard()
-  {
-        /**
-     * The following code interacts with the REV Digit Board. By default, the
-     * current battery voltage is displayed on the digit board. Pushing the A button
-     * cycles through the autonomous options.
-     */
-    if (digitBoard.getButtonA() != buttonA) {
-      buttonA = !buttonA;
-      if (buttonA) {
-        if (++optionIndex >= options.length) {
-          optionIndex = 0;
-        }
-        SmartDashboard.putString("Autonomous", options[optionIndex]);
-      }
-    }
+	/**
+	 * Polls the digit board.
+	 */
+	private void pollDigitBoard()
+	{
+		/**
+		 * The following code interacts with the REV Digit Board. By default, the
+		 * current battery voltage is displayed on the digit board. Pushing the A button
+		 * cycles through the autonomous options.
+		 */
+		if (digitBoard.getButtonA() != buttonA)
+		{
+			buttonA = !buttonA;
+			if (buttonA)
+			{
+				if (++optionIndex >= options.length)
+				{
+					optionIndex = 0;
+				}
+				SmartDashboard.putString("Autonomous", options[optionIndex]);
+			}
+		}
 
-    if (digitBoard.getButtonB() != buttonB) {
-      buttonB = !buttonB;
-    }
+		if (digitBoard.getButtonB() != buttonB)
+		{
+			buttonB = !buttonB;
+		}
 
-    if (buttonA) {
-      digitBoard.display(options[optionIndex]);
-    } else if (buttonB) {
-      if (++potRefreshCounter > potRefreshCount) {
-        potRefreshCounter = 0;
-      }
-      if (potRefreshCounter == 0) {
-        digitBoard.display(digitBoard.getPot());
-      }
-    } else {
-      if (++voltageRefreshCounter > voltageRefreshCount) {
-        voltageRefreshCounter = 0;
-      }
-      if (voltageRefreshCounter == 0) {
-        digitBoard.display(RobotController.getBatteryVoltage());
-      }
-    }
-  }
+		if (buttonA)
+		{
+			digitBoard.display(options[optionIndex]);
+		} else if (buttonB)
+		{
+			if (++potRefreshCounter > potRefreshCount)
+			{
+				potRefreshCounter = 0;
+			}
+			if (potRefreshCounter == 0)
+			{
+				digitBoard.display(digitBoard.getPot());
+			}
+		} else
+		{
+			if (++voltageRefreshCounter > voltageRefreshCount)
+			{
+				voltageRefreshCounter = 0;
+			}
+			if (voltageRefreshCounter == 0)
+			{
+				digitBoard.display(RobotController.getBatteryVoltage());
+			}
+		}
+	}
 
-  /**
-   * Polls the Limelight vision processing camera.
-   */
-  private void pollLimelight()
-  {
-    //read values periodically
-    limelightXPosition = limelightXPositionEntry.getDouble(0.0);
-    limelightYPosition = limelightYPositionEntry.getDouble(0.0);
-    limelightArea = limelightAreaEntry.getDouble(0.0);
+	/**
+	 * Polls the Limelight vision processing camera.
+	 */
+	private void pollLimelight()
+	{
+		// read values periodically
+		limelightXPosition = limelightXPositionEntry.getDouble(0.0);
+		limelightYPosition = limelightYPositionEntry.getDouble(0.0);
+		limelightArea = limelightAreaEntry.getDouble(0.0);
 
-    //post to smart dashboard periodically
-    SmartDashboard.putNumber("LimelightX", limelightXPosition);
-    SmartDashboard.putNumber("LimelightY", limelightYPosition);
-    SmartDashboard.putNumber("LimelightArea", limelightArea);
-  }
+		// post to smart dashboard periodically
+		SmartDashboard.putNumber("LimelightX", limelightXPosition);
+		SmartDashboard.putNumber("LimelightY", limelightYPosition);
+		SmartDashboard.putNumber("LimelightArea", limelightArea);
+	}
 }
--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -13,20 +13,23 @@ package frc.robot;
  * the wiring easier and significantly reduces the number of magic numbers
  * floating around.
  */
-public class RobotMap {
-  // For example to map the left and right motors, you could define the
-  // following variables to use with your drivetrain subsystem.
-  // public static int leftMotor = 1;
-  // public static int rightMotor = 2;
+public class RobotMap
+{
+	// For example to map the left and right motors, you could define the
+	// following variables to use with your drivetrain subsystem.
+	// public static int leftMotor = 1;
+	// public static int rightMotor = 2;
 
-  // If you are using multiple modules, make sure to define both the port
-  // number and the module. For example you with a rangefinder:
-  // public static int rangefinderPort = 1;
-  // public static int rangefinderModule = 1;
+	// If you are using multiple modules, make sure to define both the port
+	// number and the module. For example you with a rangefinder:
+	// public static int rangefinderPort = 1;
+	// public static int rangefinderModule = 1;
 
-  // The following deal with the Limelight vision processing camera:
-  public static String limelightTableKey = "limelight-johnrk";
-  public static String limelightXPositionKey = "tx";
-  public static String limelightYPositionKey = "ty";
-  public static String limelightAreaKey = "ta";
+	// The following deal with the Limelight vision processing camera:
+	public static String limelightTableKey = "limelight-johnrk";
+	public static String limelightXPositionKey = "tx";
+	public static String limelightYPositionKey = "ty";
+	public static String limelightAreaKey = "ta";
+	public static String limelightCamModeKey = "camMode";
+	public static String limelightLedModeKey = "ledMode";
 }
